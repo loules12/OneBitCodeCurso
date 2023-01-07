82e41d2ec3e2ac849819d079155be4052bd8024b
Basílio Neto <basiliobsneto@gmail.com>
2023-01-07 15:30:54 -0300

    Fila de Espera

--- EXAMPLES/JavaScript/exercicio_11_JS/fila_de_espera.js
+++ EXAMPLES/JavaScript/exercicio_11_JS/fila_de_espera.js
@@ -1,36 +1,51 @@
-let opcao = 0
-const fila = []
-let filaOrdenada = []
-let filaString = ""
-let itemAtual = ""
-let novoPaciente = ""
-let pacienteConsulta = ""
+let opcao = 0;
+const fila = [];
+let filaOrdenada = [];
+let filaString = "";
+let novoPaciente = "";
+let pacienteConsulta = "";
 
-do{
-  opcao = parseFloat(prompt(filaString + "Escolha uma opção: \n1)Novo paciente\n2)Consultar paciente\n3)Sair"))
-  switch(opcao){
+do {
+  opcao = parseFloat(
+    prompt(
+      filaString.replaceAll(",", "") +
+        "Escolha uma opção: \n1)Novo paciente\n2)Consultar paciente\n3)Sair"
+    )
+  );
+  switch (opcao) {
     case 1:
-      novoPaciente = prompt("Informe o nome do novo paciente:")
-      fila.push(novoPaciente)
-      filaOrdenada.push((fila.indexOf(novoPaciente) + 1) + "º - " + novoPaciente + "\n")
-      filaString = filaOrdenada.toString()
-      break
+      novoPaciente = prompt("Informe o nome do novo paciente:");
+      fila.push(novoPaciente);
+      filaOrdenada.push(
+        fila.indexOf(novoPaciente) + 1 + "º - " + novoPaciente + "\n"
+      );
+      filaString = filaOrdenada.toString();
+      break;
     case 2:
-      pacienteConsulta = fila.shift()
-      alert("Paciente " + pacienteConsulta + " vai ser atendido agora!")
-      filaOrdenada.shift()
+      if (filaOrdenada.length === 0) {
+        alert("Não temos pacientes cadastrados no momento!");
+        break;
+      } else {
+        pacienteConsulta = fila.shift();
+        alert("Paciente " + pacienteConsulta + " vai ser atendido agora!");
+        filaOrdenada.shift();
 
-      for(let i = 0; i < filaOrdenada.length; i++) {
-        itemAtual = filaOrdenada[i]
-        filaOrdenada.splice(i, 1, ((fila.indexOf(itemAtual) + 1) + "º - " + itemAtual + "\n"))
-      } 
-      filaString = filaOrdenada.toString().replaceAll(",", "")
-      break
+        for (let i = 0; i < filaOrdenada.length; i++) {
+          filaOrdenada.splice(
+            i,
+            1,
+            fila.indexOf(fila[i]) + 1 + "º - " + fila[i] + "\n"
+          );
+        }
+
+        filaString = filaOrdenada.toString();
+      }
+      break;
     case 3:
-      alert("Finalizando")
-      break
+      alert("Finalizando");
+      break;
     default:
-      alert("Está não é uma opção valida, tente novamente!")
-      break
+      alert("Está não é uma opção valida, tente novamente!");
+      break;
   }
-}while(opcao != 3)
+} while (opcao != 3);
